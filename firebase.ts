ec370d08df3c093d00274f1ba7d80e04011de970
max-lee-dev <maxplee8@gmail.com>
2025-01-25 01:54:30 -0500

    🥛 omg add api;

--- firebase.ts
+++ firebase.ts
@@ -3,14 +3,15 @@ import { connectFirestoreEmulator, getFirestore } from "@firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXlE4jNzAxnNuOzQz0gVk6wPFgJ0xph8k",
-  authDomain: "helpdad.firebaseapp.com",
-  projectId: "helpdad",
-  storageBucket: "helpdad.appspot.com",
-  messagingSenderId: "150569758046",
-  appId: "1:150569758046:web:86d62db4bc2a80044073cf",
-  measurementId: "G-P61R0TBTZS"
+  apiKey: "AIzaSyBGqZ7eegcJmZ10cel2Z0kcE-gGuJytiBo",
+  authDomain: "helpdad-96fe3.firebaseapp.com",
+  projectId: "helpdad-96fe3",
+  storageBucket: "helpdad-96fe3.firebasestorage.app",
+  messagingSenderId: "895420914515",
+  appId: "1:895420914515:web:480de1c3527f21e82b9fa6",
+  measurementId: "G-PZ8PEDBE2J"
 };
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp)
